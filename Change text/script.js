dc4a978e53b831c75a6636a75c300d44d4157f0e
Vladyslav <itochanenko@gmail.com>
2018-12-21 20:28:11 +0200

    Tested arrays

--- Change text/script.js
+++ Change text/script.js
@@ -1,31 +1,32 @@
 //  Function to change text in <p> tag
 function changeName() {
-    document.getElementById("text-item").innerHTML = 'Hello World!';
+    document.getElementById("text-item").innerHTML = 'Hello World!'
 }
 
 //  Enlarge text
 function enlargeText() {
-    var textElement = document.getElementById("text-size");
-    var style = window.getComputedStyle(textElement, null).getPropertyValue('font-size');
-    textElement.style.fontSize = ((parseFloat(style) + 1) + 'px');
+    var textElement = document.getElementById("text-size")
+    var style = window.getComputedStyle(textElement, null).getPropertyValue('font-size')
+    textElement.style.fontSize = ((parseFloat(style) + 1) + 'px')
 }
 
 //  Not enlarge text
 function notEnlargeText() {
-    var textElement = document.getElementById("text-size");
-    var style = window.getComputedStyle(textElement, null).getPropertyValue('font-size');
-    var newSize = parseFloat(style);
+    var textElement = document.getElementById("text-size")
+    var style = window.getComputedStyle(textElement, null).getPropertyValue('font-size')
+    var newSize = parseFloat(style)
     if (newSize <= 1) {
-        newSize = 1 + 'px';
-        alert('font-size is too small!');
+        newSize = 1 + 'px'
+        alert('font-size is too small!')
     }
-    textElement.style.fontSize = ((newSize - 1) + 'px');
+    textElement.style.fontSize = ((newSize - 1) + 'px')
 }
 
 function writeArray() {
     var str = "";
-    for(var i = 0; i < 100; i++) {
-        str += i + '<br>';
+    whatIWant = ['cheese', 'relationship', 'to sleep']
+    for(var i = 0; i < 3; i++) {
+        str += whatIWant[i] + '<br>'
     }
-    document.getElementById("write-here").innerHTML = str;
+    document.getElementById("write-here").innerHTML = str
 }
