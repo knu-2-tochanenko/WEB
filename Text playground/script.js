8f6c339586212decd8de80de3e52f15c99fd0dc5
Vladyslav <itochanenko@gmail.com>
2019-01-01 03:52:24 +0200

    Refactored code, added festive background, updated styles

--- Text playground/script.js
+++ Text playground/script.js
@@ -1,26 +1,24 @@
-function loadPage() {
-    document.getElementById("string-type").innerHTML = typeof("Hello");
-    document.getElementById("number-type").innerHTML = typeof(65536);
+//  Load page context after loading page
+window.onload = function() {
+    document.getElementById('string-type').innerHTML = typeof('Hello');
+    document.getElementById('number-type').innerHTML = typeof(65536);
 }
 
-//  Load page context after loading page
-window.onload = loadPage;
-
 //  Function to change text in <p> tag
-function changeName() {
-    document.getElementById("text-item").innerHTML = 'Hello World!';
+document.getElementById('typeItButton').onclick = function() {
+    document.getElementById('text-item').innerHTML = 'Hello World!';
 }
 
 //  Enlarge text
-function enlargeText() {
-    var textElement = document.getElementById("text-size");
+document.getElementById('enlargeText').onclick = function() {
+    var textElement = document.getElementById('text-size');
     var style = window.getComputedStyle(textElement, null).getPropertyValue('font-size');
     textElement.style.fontSize = ((parseFloat(style) + 1) + 'px');
 }
 
 //  Not enlarge text
-function notEnlargeText() {
-    var textElement = document.getElementById("text-size");
+document.getElementById('notEnlargeText').onclick = function() {
+    var textElement = document.getElementById('text-size');
     var style = window.getComputedStyle(textElement, null).getPropertyValue('font-size');
     var newSize = parseFloat(style);
     if (newSize <= 1) {
@@ -30,64 +28,64 @@ function notEnlargeText() {
     textElement.style.fontSize = ((newSize - 1) + 'px');
 }
 
-function writeArray() {
-    var str = "";
+document.getElementById('writeArray').onclick = function() {
+    var str = '';
     whatIWant = ['cheese', 'relationship', 'to sleep'];
     for(var i = 0; i < 3; i++) {
         str += whatIWant[i] + '<br>';
     }
-    document.getElementById("write-here").innerHTML = str;
+    document.getElementById('write-here').innerHTML = str;
 }
 
-function hideName() {
-    document.getElementById("text-item").style.display = "none"
+document.getElementById('hideName').onclick = function() {
+    document.getElementById('text-item').style.display = 'none'
 }
 
-function showName() {
-    document.getElementById("text-item").style.display = "block"
+document.getElementById('showName').onclick = function() {
+    document.getElementById('text-item').style.display = 'block'
 }
 
-function calculate() {
-    var firstNumber = parseInt(document.getElementById("first-number").value);
-    var secondNumber = parseInt(document.getElementById("second-number").value);
-    var dropDown = document.getElementById("operation");
+document.getElementById('calculate').onclick = function() {
+    var firstNumber = parseInt(document.getElementById('first-number').value);
+    var secondNumber = parseInt(document.getElementById('second-number').value);
+    var dropDown = document.getElementById('operation');
     var selectedValue = dropDown[dropDown.selectedIndex].text;
     var res;
     switch (selectedValue) {
-    case "+":
+    case '+':
         res = firstNumber + secondNumber;
         break;
-    case "-":
+    case '-':
         res = firstNumber - secondNumber;
         break;
-    case "*":
+    case '*':
         res = firstNumber * secondNumber;
         break;
-    case "/":
+    case '/':
         if (secondNumber == 0) {
-            window.alert("Can't divide by zero!");
-            console.log("Something went wrong in '/' method");
-            res = "Try again";
+            window.alert('Can\'t divide by zero!');
+            console.log('Something went wrong in '/' method');
+            res = 'Try again';
         }
         else
             res = firstNumber / secondNumber;
         break;
-    case "!":
+    case '!':
         res = 1;
         for (i = 2; i <= firstNumber; i++)
             res *= i;
         break;
-    case "%":
+    case '%':
         if (secondNumber == 0) {
-            window.alert("Can't divide by zero!");
-            console.log("Something went wrong in '%' method");
-            res = "Try again";
+            window.alert('Can\'t divide by zero!');
+            console.log('Something went wrong in '%' method');
+            res = 'Try again';
         }
         else
             res = firstNumber % secondNumber;
         break;
     default:
-        res = "Try again";
+        res = 'Try again';
     }
-    document.getElementById("res").innerHTML = res;
+    document.getElementById('res').innerHTML = res;
 }
